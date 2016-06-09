b3aa162ace8530b9cd1074a7d99ce08f25188842
Dave Mackey <Dave Mackey>
2016-06-09 12:31:31 -0400

    Initial Commit

---
+++ liquid_template.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+* Template Name:Liquid Template
+ *The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Liquid_Church
+ * @since Liquid Church 1.0
+ */
+
+get_header(); ?>
+
+<div class="body_content">
+	<main id="main" class="site-main" role="main">
+		<?php
+	
+			echo do_shortcode( '[SHOW_HEADER_BANNER_SECTION]' );
+				while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'template' );
+
+		
+
+			// End of the loop.
+		endwhile;
+
+			echo do_shortcode( '[SHOW_PAGE_SECTION]' );
+		
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .Body-Content -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ template-parts/content-single-gc-sermons.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template part for displaying single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+        <?php
+        $sermon = gc_get_sermon_post( $sermon_post_id );
+        $video_player = $sermon->get_video_player();
+        echo $video_player;
+        $excerpt = $sermon->loop_excerpt();
+        echo $excerpt;
+        do_action( 'gc_sermons', array(
+            'per_page' => 5,
+            'related_series' => 'this',
+            'content' => '',
+            'thumbnail_size' => 'medium',
+            'number_columns' => '4',
+        ) );
+        ?>
+    </div><!-- .entry-content -->
+
+    <footer class="entry-footer">
+        <?php twentysixteen_entry_meta(); ?>
+        <?php
+        edit_post_link(
+            sprintf(
+            /* translators: %s: Name of current post */
+                __( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+                get_the_title()
+            ),
+            '<span class="edit-link">',
+            '</span>'
+        );
+        ?>
+    </footer><!-- .entry-footer -->
+</article><!-- #post-## -->
