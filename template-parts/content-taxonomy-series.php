ce8911e39be005809f4315ba93ecb41b3230e635
Dave Mackey <davem@liquidchurch.com>
2016-06-14 03:05:14 -0400

    - Image, sermon title, and names all now appearing correctly.

--- template-parts/content-taxonomy-series.php
+++ template-parts/content-taxonomy-series.php
@@ -9,10 +9,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
 
 	<?php
 	?>
@@ -21,10 +18,35 @@
 		<?php
 		$sermon = gc_get_sermon_post( $sermon_post_id );
 		?>
-		<a href="<?php echo $sermon->permalink();?>">
-			<?php echo wp_get_attachment_image( $sermon->featured_image_id(), 'full', false, array(
-			'class' => 'gc-series-list-sermons-img', ) ); ?>
-		</a>
+		<div class="row">
+			<div class="col-md-5">
+				<a href="<?php echo $sermon->permalink();?>">
+					<?php echo wp_get_attachment_image( $sermon->featured_image_id(), 'full', false, array(
+						'class' => 'gc-series-list-sermons-img', ) ); ?>
+				</a>
+			</div>
+			<div class="col-md-7">
+				<header class="entry-header">
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				</header><!-- .entry-header -->
+				<?php
+				$speaker = $sermon->get_speaker();
+				echo $speaker->name;
+				//$speakers = gc_sermons()->taxonomies->speakers->get( get_queried_object_id() );
+				//echo $speakers->name;
+				//$speakers = $sermon->get_speaker();
+				//foreach ($speakers as $key => $speaker) {
+				/*	echo $key . " <br />";
+					echo $speaker . " <br />";
+					$name = $speaker->name;
+					echo $name;
+
+				} */
+				?>
+			</div>
+		</div>
+
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
