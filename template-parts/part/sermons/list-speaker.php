827f73786167da17aef9aa9c144a224f07ace45b
Dave Mackey <davem@liquidchurch.com>
2016-09-03 21:55:59 -0400

    - Updates to the sermon management templating.

---
+++ template-parts/part/sermons/list-speaker.php
@@ -0,0 +1,23 @@
+<?php
+global $sermon;
+$speakers = $sermon->get_speakers();
+if (empty($speakers))
+    return false;
+?>
+<div id="message-speaker" class="row">
+    <div class="col-sm-3">
+        <b>Speaker:</b>
+    </div>
+    <div class="col-sm-9">
+        <?php
+        $speaker = array();
+        foreach ($speakers as $key => $val) {
+            $speaker[] = $val->name;
+        }
+        echo implode(', ', $speaker);
+
+        /* TODO: Link the name of the speaker.
+        echo $speaker_url->slug; */
+        ?>
+    </div>
+</div>
---
+++ template-parts/part/sermons/list-topics.php
@@ -0,0 +1,20 @@
+<?php
+global $sermon;
+$topics = $sermon->topics();
+if (empty($topics))
+    return false;
+?>
+<div id="message-topics" class="row">
+    <div class="col-sm-3">
+        <b>Topic:</b>
+    </div>
+    <div class="col-sm-9">
+        <?php
+        $topic = array();
+        foreach ($topics as $key => $val) {
+            $topic[] = $val->name;
+        }
+        echo implode(', ', $topic);
+        ?>
+    </div>
+</div>
