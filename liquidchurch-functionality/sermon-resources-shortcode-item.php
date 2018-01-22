e7807b25441c57773b06cde648ae69adc0ddae3c
Dave Mackey <davem@liquidchurch.com>
2018-01-22 16:23:50 -0500

    - Removed a lot of gc-sermons and lqd-functionality templates that didn't differ from the actual plugins. If we change them in the future, we can always re-add. - Added single-post.php which was crated in order to handle blog posts for love week. - Added template-messages-app-view.php and template-parts/content-messages-app-view.php which were created to handle app view. Still in process.

--- liquidchurch-functionality/sermon-resources-shortcode-item.php
+++
@@ -1,7 +0,0 @@
-<a target="_blank" href="<?php $this->output( 'file', 'esc_url' ); ?>">
-	<?php if ( $this->get( 'do_display_name' ) ) : ?>
-		<?php $this->output( 'display_name' ); ?>
-	<?php else : ?>
-		<?php $this->output( 'name' ); ?>
-	<?php endif; ?>
-</a>
--- template-messages.php
+++ template-messages.php
@@ -3,10 +3,6 @@
  * Template Name: Messages
  * The template for displaying messages
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
  * @package WordPress
  * @subpackage Liquid_Church
  * @since Liquid Church 1.0
