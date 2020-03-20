33cd982b56f2e9890624b19a267df57afce0bd18
Dave Mackey <dave@davemackey.net>
2020-03-20 18:15:26 -0400

    - Remove api var and replace with full wp.customize calls.

--- js/customize-preview.js
+++ js/customize-preview.js
@@ -3,38 +3,35 @@
  */
 
 ( function( $ ) {
-	var style = $( '#liquidchurch-color-scheme-css' ),
-		api = wp.customize;
+	var style = $( '#liquidchurch-color-scheme-css' );
 
 	if ( ! style.length ) {
 		style = $( 'head' ).append( '<style type="text/css" id="liquidchurch-color-scheme-css" />' )
 		                    .find( '#liquidchurch-color-scheme-css' );
 	}
 
-	// Site title.
-	api( 'blogname', function( value ) {
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-title a' ).text( to );
 		} );
 	} );
-
-	// Site tagline.
-	api( 'blogdescription', function( value ) {
+	wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
 		} );
 	} );
 
 	// Add custom-background-image body class when background image is added.
-	api( 'background_image', function( value ) {
+	wp.customize( 'background_image', function( value ) {
 		value.bind( function( to ) {
 			$( 'body' ).toggleClass( 'custom-background-image', '' !== to );
 		} );
 	} );
 
 	// Color Scheme CSS.
-	api.bind( 'preview-ready', function() {
-		api.preview.bind( 'update-color-scheme-css', function( css ) {
+	wp.customize.bind( 'preview-ready', function() {
+		wp.customize.preview.bind( 'update-color-scheme-css', function( css ) {
 			style.html( css );
 		} );
 	} );
