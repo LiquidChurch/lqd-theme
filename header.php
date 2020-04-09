e2657ed3fff5325569eb2fe097842008e3048ae8
Dave Mackey <dave@davemackey.net>
2020-04-09 15:42:14 -0400

    - Modify layout of COVID Relief header.

--- header.php
+++ header.php
@@ -25,10 +25,10 @@
     <!-- Header start -->
     <div class="lqd-header container">
         <div class="lqd-header-covid row">
-            <div class="col-xs-12 col-lg-9 text-center display-flex">
+            <div class="col-xs-12 col-lg-7 col-lg-offset-1 display-flex">
                 <p class="lqd-header-covid-text"><strong>Hope Is On The Way!</strong> Get Help - Receive A Free Emergency Relief Kit.</p>
             </div>
-            <div class="col-xs-12 col-lg-2 col-lg-offset-1 display-flex">
+            <div class="col-xs-12 col-lg-2 col-lg-offset-2 display-flex">
                 <a class="btn lqd-header-covid-btn" href="https://liquidchurch.com/relief">COVID-19 RELIEF</a>
             </div>
         </div>
