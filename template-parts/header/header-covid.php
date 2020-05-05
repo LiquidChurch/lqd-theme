768f04c3a37d3c0bd742c2c8821946aa2e0cb972
Dave Mackey <dave@davemackey.net>
2020-05-05 10:55:32 -0400

    - Removing old button styling. - Adding background color for new header button styling. - Improving responsiveness of COVID and header top buttons. - Consolidating logic for if..else in header-top.php

--- template-parts/header/header-covid.php
+++ template-parts/header/header-covid.php
@@ -3,6 +3,6 @@
         <span class="lqd-header-covid-text"><strong>Hope Is On The Way!</strong> Get Help - Receive A Free Emergency Relief Kit.</span>
     </div>
     <div class="col-xs-12 col-md-3">
-        <a class="lqd-header-covid-btn-no btn btn-lg btn-primary float-right" style="background-color: #14a7e0;" href="/relief">COVID-19 RELIEF</a>
+        <a class="lqd-header-covid-btn-no btn btn-lg btn-primary float-md-right" style="background-color: #14a7e0;" href="/relief">COVID-19 RELIEF</a>
     </div>
 </div>
--- template-parts/header/header-top.php
+++ template-parts/header/header-top.php
@@ -2,18 +2,16 @@
     <div class="lqd-header-logo col-xs-12 col-md-8">
         <?php if (get_theme_mod( 'm1_logo' )){ ?>
             <a href="<?php echo home_url('/')?>"><img src="<?php echo get_theme_mod( 'm1_logo' ) ;?>" width="220" height="40" alt=""></a>
-            <?php
-        } else {
-            if ( is_front_page() && is_home() ) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php endif; } ?>
+        <?php } else if ( is_front_page() && is_home() ) : ?>
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <?php else : ?>
+            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+        <?php endif; ?>
     </div>
-    <div class="lqd-header-church-online col-xs-8 col-xs-offset-2 col-md-3 col-md-offset-0">
-        <a class="lqd-header-btn-no btn btn-primary btn-lg" style="background-color:#14a7e0" href="https://liquidchurchonline.com/">Church Online</a>
+    <div class="lqd-header-church-online col-xs-8 mb-2 col-md-3 mb-md-0">
+        <a class="lqd-header-btn btn btn-primary btn-lg" href="https://liquidchurchonline.com/">Church Online</a>
     </div>
-    <div class="lqd-header-give col-xs-8 col-xs-offset-2 col-md-1 col-md-offset-0">
-        <a class="lqd-header-btn-no btn btn-primary btn-lg" style="background-color:#14a7e0" href="/give">Give</a>
+    <div class="lqd-header-give col-xs-4 mb-2 col-md-1 mb-md-0">
+        <a class="lqd-header-btn btn btn-primary btn-lg" href="/give">Give</a>
     </div>
 </div>
